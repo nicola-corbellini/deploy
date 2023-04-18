158096a6dad27712ffc472eb0bec081a13a61dc5
Nicola <nicorb932@hotmail.com>
2023-04-18 22:55:41 +0100

    prova

--- admin/src/config.ts
+++ admin/src/config.ts
@@ -14,10 +14,10 @@ const config: Config = {
     AppFeatures.Plugins
   ],
   endpoints: {
-    chat: 'ws://87.14.7.41:1865/ws',
-    rabbitHole: 'http://87.14.7.41:1865/rabbithole',
-    allLLM: 'http://87.14.7.41:1865/settings/llm/',
-    singleLLM: 'http://87.14.7.41:1865/settings/llm/:llm'
+    chat: 'ws://192.168.1.68:1865/ws',
+    rabbitHole: 'http://192.168.1.68:1865/rabbithole',
+    allLLM: 'http://192.168.1.68:1865/settings/llm/',
+    singleLLM: 'http://192.168.1.68:1865/settings/llm/:llm'
   }
 }
 
